8038ddf7592d652787a363d9df2f0a18aaa45b1f
Jakub Szewczyk <jakubsz3wczyk@gmail.com>
2022-04-07 23:57:28 +0200

    updated to changes in auth paths

--- src/components/UserAuth.vue
+++ src/components/UserAuth.vue
@@ -58,7 +58,7 @@ export default {
   },
   methods: {
     signUp () {
-      $.post(`http://${this.$backend}/auth/users/create/`, this.$data, (data) => {
+      $.post(`http://${this.$backend}/auth/users/`, this.$data, (data) => {
         this.signIn()
       })
         .fail((response) => {
@@ -69,7 +69,7 @@ export default {
     signIn () {
       const credentials = {username: this.username, password: this.password}
 
-      $.post(`http://${this.$backend}/auth/token/create/`, credentials, (data) => {
+      $.post(`http://${this.$backend}/auth/token/login/`, credentials, (data) => {
         sessionStorage.setItem('authToken', data.auth_token)
         sessionStorage.setItem('username', this.username)
         this.$router.push('/')
